e77dbfaa55569aa25f18f9381f949730393b6e57
thechild <chris@bigmac.local>
2015-11-18 10:51:23 -0800

    Added login/logout logging to segment

---
+++ Gifts/signals.py
@@ -0,0 +1,24 @@
+import analytics
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
+import logging
+
+
+# auth completed signal #
+@receiver(user_logged_in)
+def login_log(sender, **kwargs):
+    print "user logged in"
+    user = kwargs['user']
+    analytics.identify(user.id, {
+        'email': user.email,
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'username': user.get_username()
+    })
+
+
+@receiver(user_logged_out)
+def logout_log(sender, **kwargs):
+    if 'user' in kwargs:
+        user = kwargs['user']
+        analytics.track(user.id, 'Logged Out')
